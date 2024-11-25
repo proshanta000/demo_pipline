afcf4255e005c5e133c47383a4f196d7eba39c45
proshanta000 <proshanta.mithu5@gmail.com>
2024-11-26 03:25:54 +0600

    all file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(name="census_income",
+      version="0.0.1",
+      author="Mithu",
+      author_email="proshanta.mithu5@gmail.com",
+      packages=find_packages(),
+      install_requires=["pandas","numpy","flask"]
+      )
